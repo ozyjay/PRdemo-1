ccd133bc92077c3a134e717b3fbb936f985bb94e
Jason Holdsworth <ozyjay@gmail.com>
2015-10-08 11:52:26 +1000

    We fixed your code Lindsay :P :)

--- demoValidity.py
+++ demoValidity.py
@@ -1,19 +1,23 @@
 """
-CP1404 example - ckecking valid numbers
+CP1404 example - checking valid numbers
 """
 __author__ = 'Lindsay Ward'
+
+
 def get_valid_number(lower, upper):
-    valid = (False)
+    valid = False
+    x = 0
     while not valid:
         try:
             x = int(input("Enter number: "))
-            if x < lower or x> upper:
+            if x < lower or x > upper:
                 print("Number is out of range")
-        except:
+        except ValueError:
             print("Number is bad")
     return x
 
+
 # test
 age = get_valid_number(0, 120)
-print("You are",age,"years old")
-    print("Finished")
+print("You are", age, "years old")
+print("Finished")
